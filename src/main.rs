dc20d5826471829ba865bf4b17c5a557e47ec4b9
Ravi Dev Pandey <pandeyravidev@gmail.com>
2024-03-01 01:08:39 +0530

    add boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use sdl2::{event::Event, pixels::Color, rect::Rect};
+use sdl2::{
+    event::Event,
+    keyboard::Keycode,
+    pixels::Color,
+    rect::Rect,
+    sys::{KeyCode, SDL_KeyCode},
+};
 
 fn main() -> Result<(), String> {
     let (scr_width, scr_height) = (640, 480);
@@ -21,20 +27,44 @@ fn main() -> Result<(), String> {
     let mut running = true;
     let mut event_queue = sdl_context.event_pump().unwrap();
 
+    let mut buf: Vec<Vec<i32>> = vec![vec![0; 640]; 480];
+
     while running {
+        canvas.set_draw_color(Color::RGB(200, 200, 200));
+        canvas.fill_rect(screen_area)?;
 
         for event in event_queue.poll_iter() {
             match event {
                 Event::Quit { .. } => running = false,
-                _ => {},
+                Event::MouseMotion {
+                    mousestate, x, y, ..
+                } => {
+                    // println!("{:?}", mousestate.left());
+                    if mousestate.left() {
+                        buf[((y / 4) * 4) as usize][((x / 4) * 4) as usize] = 1;
+                    }
+                }
+                Event::KeyDown {
+                    keycode,
+                    scancode,
+                    keymod,
+                    ..
+                } => if keycode.unwrap() == Keycode::R {},
+                _ => {}
             }
         }
 
-        canvas.set_draw_color(Color::RGB(200, 200, 200));
-        canvas.fill_rect(screen_area)?;
-
         canvas.set_draw_color(Color::RGB(20, 20, 20));
-        canvas.fill_rect(Rect::new(100, 100, 100, 100))?;
+
+        for y in 0..buf.len() {
+            for x in 0..buf[0].len() {
+                if buf[y][x] == 1 {
+                    canvas.fill_rect(Rect::new(x as i32, y as i32, 4, 4))?;
+                }
+            }
+        }
+
+        // canvas.fill_rect(Rect::new(100, 100, 100, 100))?;
 
         canvas.present();
     }
