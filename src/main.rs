f0533092fce1aafcf9dc328a2424e2ab84830cca
Ravi Dev Pandey <pandeyravidev@gmail.com>
2024-03-02 13:23:44 +0530

    isolate RPS in its module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,52 @@
 mod rock_paper_scissor;
 
-use std::cmp::min;
+use rock_paper_scissor::{BufData, RockPaperScissor};
 
-use rand::Rng;
 use sdl2::{
-    event::Event,
-    keyboard::Keycode,
     pixels::Color,
-    rect::{Point, Rect},
+    rect::Rect,
+    render::Canvas,
+    video::Window,
 };
 
-#[derive(Clone, Copy)]
-struct BufData {
-    color: i32,
-    intensity: i32,
+struct GlobalContext<'a> {
+    scr_width: u32,
+    scr_height: u32,
+    grid_width: i32,
+    grid_height: i32,
+    block_size: i32,
+    screen_area: Rect,
+    bg_color: Color,
+    running: bool,
+    canvas: &'a mut Canvas<Window>,
 }
 
-fn in_bounds(y: &i32, x: &i32, grid_h: &i32, grid_w: &i32) -> bool {
-    y >= &0 && x >= &0 && y < grid_h && x < grid_w
+impl<'a> GlobalContext<'a> {
+    fn new(
+        scr_w: u32,
+        scr_h: u32,
+        block_size: i32,
+        bg_color: Color,
+        canvas: &'a mut Canvas<Window>,
+    ) -> Self {
+        GlobalContext {
+            scr_width: scr_w,
+            scr_height: scr_h,
+            block_size,
+            grid_width: scr_w as i32 / block_size,
+            grid_height: scr_h as i32 / block_size,
+            screen_area: Rect::new(0, 0, scr_w, scr_h),
+            bg_color,
+            running: true,
+            canvas,
+        }
+    }
 }
 
 fn main() -> Result<(), String> {
-    let (scr_width, scr_height) = (1080, 720);
-    let block_size: i32 = 8;
-    let max_intensity: i32 = 5;
-
-    let (grid_width, grid_height) = (
-        scr_width as i32 / block_size,
-        scr_height as i32 / block_size,
-    );
-
-    let background_color = Color::RGBA(197, 214, 220, 255);
+    let scr_width = 1080;
+    let scr_height = 720;
+    let bg_color = Color::RGBA(197, 214, 220, 255);
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -40,151 +56,31 @@ fn main() -> Result<(), String> {
         .build()
         .expect("Failed to build window");
 
-    let mut rand_gen = rand::thread_rng();
-
-    let screen_area = Rect::new(0, 0, scr_width, scr_height);
-
     let mut canvas = window
         .into_canvas()
         .build()
         .expect("Failed to build context");
 
-    canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
+    let mut global_c = GlobalContext::new(1080, 720, 8, bg_color, &mut canvas);
+    global_c
+        .canvas
+        .set_blend_mode(sdl2::render::BlendMode::Blend);
 
-    let mut running = true;
     let mut event_queue = sdl_context.event_pump().unwrap();
-    let mut cur_draw_color = 1;
 
-    let mut buf: Vec<Vec<BufData>> = vec![
-        vec![
-            BufData {
-                color: 0,
-                intensity: max_intensity
-            };
-            grid_width as usize
-        ];
-        grid_height as usize
-    ];
+    let mut rps = RockPaperScissor::new();
+    rps.setup(&global_c);
 
-    // canvas.set_draw_color(background_color);
-    // canvas.fill_rect(screen_area)?;
-
-    while running {
-        // canvas.set_draw_color(background_color);
-        // canvas.fill_rect(screen_area)?;
+    while global_c.running {
+        global_c.canvas.set_draw_color(global_c.bg_color);
+        global_c.canvas.fill_rect(global_c.screen_area)?;
 
         for event in event_queue.poll_iter() {
-            match event {
-                Event::Quit { .. } => running = false,
-                Event::MouseMotion {
-                    mousestate, x, y, ..
-                } => {
-                    // println!("{:?}", mousestate.left());
-                    if mousestate.left() {
-                        buf[(y / block_size) as usize][(x / block_size) as usize].color =
-                            cur_draw_color;
-                    }
-                }
-                Event::KeyDown { keycode, .. } => match keycode.unwrap() {
-                    Keycode::R => cur_draw_color = 1,
-                    Keycode::G => cur_draw_color = 2,
-                    Keycode::B => cur_draw_color = 3,
-                    _ => cur_draw_color = 0,
-                },
-                _ => {}
-            }
+            rps.handle_event(&event, &mut global_c);
         }
 
-        for y in 0..buf.len() {
-            for x in 0..buf[0].len() {
-                let neighbour_y = rand_gen.gen_range(-1..=1) + (y as i32);
-                let neighbour_x = rand_gen.gen_range(-1..=1) + (x as i32);
-
-                // println!("y: {:?}, x: {:?}", neighbour_y - (y as i32), neighbour_x - (x as i32));
-
-                if in_bounds(&neighbour_y, &neighbour_x, &grid_height, &grid_width) {
-                    let mut neighbour_pixel = buf[neighbour_y as usize][neighbour_x as usize];
-
-                    match buf[y][x].color {
-                        0 => {
-                            if neighbour_pixel.color != 0 && neighbour_pixel.intensity > 0 {
-                                buf[y][x].color = neighbour_pixel.color;
-                                buf[y][x].intensity = neighbour_pixel.intensity - 1;
-                            }
-                        }
-                        1 => {
-                            if neighbour_pixel.color == 2 {
-                                neighbour_pixel.color = 0;
-                                neighbour_pixel.intensity = max_intensity;
-                                buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-                                // buf[y][x].intensity = 10;
-                            }
-                        }
-                        2 => {
-                            if neighbour_pixel.color == 3 {
-                                neighbour_pixel.color = 0;
-                                neighbour_pixel.intensity = max_intensity;
-                                buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-                                // buf[y][x].intensity = 10;
-                            }
-                        }
-                        3 => {
-                            if neighbour_pixel.color == 1 {
-                                neighbour_pixel.color = 0;
-                                neighbour_pixel.intensity = max_intensity;
-                                buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-                                // buf[y][x].intensity = 10;
-                            }
-                        }
-                        _ => {}
-                    }
-
-                    buf[neighbour_y as usize][neighbour_x as usize] = neighbour_pixel;
-                }
-            }
-        }
-
-        for y in 0..buf.len() {
-            for x in 0..buf[0].len() {
-                let rect_color: Color;
-
-                match buf[y][x].color {
-                    0 => rect_color = Color::RGBA(200, 200, 200, 0),
-                    1 => rect_color = Color::RGB(246, 122, 17),
-                    2 => rect_color = Color::RGB(208, 37, 37),
-                    3 => rect_color = Color::RGB(35, 119, 181),
-                    _ => rect_color = Color::RGB(20, 20, 20),
-                }
-
-                if buf[y][x].color != 0 {
-                    canvas.set_draw_color(rect_color);
-                    canvas.fill_rect(Rect::new(
-                        (x as i32) * block_size,
-                        (y as i32) * block_size,
-                        block_size as u32,
-                        block_size as u32,
-                    ))?;
-                }
-            }
-        }
-
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-
-        for x in 0..buf[0].len() {
-            canvas.draw_line(
-                Point::new((x as i32) * block_size, (0 as i32) * block_size),
-                Point::new((x as i32) * block_size, (scr_height as i32) * block_size),
-            )?;
-        }
-
-        for y in 0..buf.len() {
-            canvas.draw_line(
-                Point::new((0 as i32) * block_size, (y as i32) * block_size),
-                Point::new((scr_width as i32) * block_size, (y as i32) * block_size),
-            )?;
-        }
-
-        canvas.present();
+        rps.draw(&mut global_c)?;
+        global_c.canvas.present();
     }
 
     Ok(())
--- src/rock_paper_scissor.rs
+++ src/rock_paper_scissor.rs
@@ -1,120 +1,186 @@
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use rand::{rngs::ThreadRng, Rng};
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::{Point, Rect}};
 use std::cmp::min;
 
-fn draw() {
-    // canvas.set_draw_color(background_color);
-    // canvas.fill_rect(screen_area)?;
+use crate::GlobalContext;
 
-    // for event in event_queue.poll_iter() {
-    //     match event {
-    //         Event::Quit { .. } => running = false,
-    //         Event::MouseMotion {
-    //             mousestate, x, y, ..
-    //         } => {
-    //             // println!("{:?}", mousestate.left());
-    //             if mousestate.left() {
-    //                 buf[(y / block_size) as usize][(x / block_size) as usize].color =
-    //                     cur_draw_color;
-    //             }
-    //         }
-    //         Event::KeyDown { keycode, .. } => match keycode.unwrap() {
-    //             Keycode::R => cur_draw_color = 1,
-    //             Keycode::G => cur_draw_color = 2,
-    //             Keycode::B => cur_draw_color = 3,
-    //             _ => cur_draw_color = 0,
-    //         },
-    //         _ => {}
-    //     }
-    // }
+#[derive(Clone, Copy)]
+pub(crate) struct BufData {
+    pub color: i32,
+    pub intensity: i32,
+}
 
-    // for y in 0..buf.len() {
-    //     for x in 0..buf[0].len() {
-    //         let neighbour_y = rand_gen.gen_range(-1..=1) + (y as i32);
-    //         let neighbour_x = rand_gen.gen_range(-1..=1) + (x as i32);
+pub(crate) struct RockPaperScissor {
+    rand_gen: ThreadRng,
+    cur_draw_color: i32,
+    max_intensity: i32,
+    pub buf: Vec<Vec<BufData>>,
+}
 
-    //         // println!("y: {:?}, x: {:?}", neighbour_y - (y as i32), neighbour_x - (x as i32));
+impl RockPaperScissor {
+    pub fn new() -> Self {
+        Self {
+            cur_draw_color: 1,
+            max_intensity: 8,
+            rand_gen: rand::thread_rng(),
+            buf: vec![vec![BufData {
+                color: 0,
+                intensity: 0,
+            }]],
+        }
+    }
 
-    //         if in_bounds(&neighbour_y, &neighbour_x, &grid_height, &grid_width) {
-    //             let mut neighbour_pixel = buf[neighbour_y as usize][neighbour_x as usize];
+    pub fn setup(&mut self, context: &GlobalContext) {
+        self.rand_gen = rand::thread_rng();
+        self.buf = vec![
+            vec![
+                BufData {
+                    color: 0,
+                    intensity: self.max_intensity
+                };
+                context.grid_width as usize
+            ];
+            context.grid_height as usize
+        ];
+    }
 
-    //             match buf[y][x].color {
-    //                 0 => {
-    //                     if neighbour_pixel.color != 0 && neighbour_pixel.intensity > 0 {
-    //                         buf[y][x].color = neighbour_pixel.color;
-    //                         buf[y][x].intensity = neighbour_pixel.intensity - 1;
-    //                     }
-    //                 }
-    //                 1 => {
-    //                     if neighbour_pixel.color == 2 {
-    //                         neighbour_pixel.color = 0;
-    //                         neighbour_pixel.intensity = max_intensity;
-    //                         buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-    //                         // buf[y][x].intensity = 10;
-    //                     }
-    //                 }
-    //                 2 => {
-    //                     if neighbour_pixel.color == 3 {
-    //                         neighbour_pixel.color = 0;
-    //                         neighbour_pixel.intensity = max_intensity;
-    //                         buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-    //                         // buf[y][x].intensity = 10;
-    //                     }
-    //                 }
-    //                 3 => {
-    //                     if neighbour_pixel.color == 1 {
-    //                         neighbour_pixel.color = 0;
-    //                         neighbour_pixel.intensity = max_intensity;
-    //                         buf[y][x].intensity = min(buf[y][x].intensity + 1, max_intensity);
-    //                         // buf[y][x].intensity = 10;
-    //                     }
-    //                 }
-    //                 _ => {}
-    //             }
+    pub fn handle_event(&mut self, event: &Event, context: &mut GlobalContext) {
+        match event {
+            Event::Quit { .. } => context.running = false,
+            Event::MouseMotion {
+                mousestate, x, y, ..
+            } => {
+                // println!("{:?}", mousestate.left());
+                if mousestate.left() {
+                    self.buf[(y / context.block_size) as usize]
+                        [(x / context.block_size) as usize]
+                        .color = self.cur_draw_color;
+                }
+            }
+            Event::KeyDown { keycode, .. } => match keycode.unwrap() {
+                Keycode::R => self.cur_draw_color = 1,
+                Keycode::G => self.cur_draw_color = 2,
+                Keycode::B => self.cur_draw_color = 3,
+                _ => self.cur_draw_color = 0,
+            },
+            _ => {}
+        }
+    }
 
-    //             buf[neighbour_y as usize][neighbour_x as usize] = neighbour_pixel;
-    //         }
-    //     }
-    // }
+    pub fn draw(&mut self, context: &mut GlobalContext) -> Result<(), String>{
+        for y in 0..self.buf.len() {
+            for x in 0..self.buf[0].len() {
+                let neighbour_y = self.rand_gen.gen_range(-1..=1) + (y as i32);
+                let neighbour_x = self.rand_gen.gen_range(-1..=1) + (x as i32);
 
-    // for y in 0..buf.len() {
-    //     for x in 0..buf[0].len() {
-    //         let rect_color: Color;
+                // println!("y: {:?}, x: {:?}", neighbour_y - (y as i32), neighbour_x - (x as i32));
 
-    //         match buf[y][x].color {
-    //             0 => rect_color = Color::RGBA(200, 200, 200, 0),
-    //             1 => rect_color = Color::RGB(246, 122, 17),
-    //             2 => rect_color = Color::RGB(208, 37, 37),
-    //             3 => rect_color = Color::RGB(35, 119, 181),
-    //             _ => rect_color = Color::RGB(20, 20, 20),
-    //         }
+                if Self::in_bounds(
+                    &neighbour_y,
+                    &neighbour_x,
+                    &context.grid_height,
+                    &context.grid_width,
+                ) {
+                    let mut neighbour_pixel = self.buf[neighbour_y as usize][neighbour_x as usize];
 
-    //         if buf[y][x].color != 0 {
-    //             canvas.set_draw_color(rect_color);
-    //             canvas.fill_rect(Rect::new(
-    //                 (x as i32) * block_size,
-    //                 (y as i32) * block_size,
-    //                 block_size as u32,
-    //                 block_size as u32,
-    //             ))?;
-    //         }
-    //     }
-    // }
+                    match self.buf[y][x].color {
+                        0 => {
+                            if neighbour_pixel.color != 0 && neighbour_pixel.intensity > 0 {
+                                self.buf[y][x].color = neighbour_pixel.color;
+                                self.buf[y][x].intensity = neighbour_pixel.intensity - 1;
+                            }
+                        }
+                        1 => {
+                            if neighbour_pixel.color == 2 {
+                                neighbour_pixel.color = 0;
+                                neighbour_pixel.intensity = self.max_intensity;
+                                self.buf[y][x].intensity =
+                                    min(self.buf[y][x].intensity + 1, self.max_intensity);
+                                // buf[y][x].intensity = 10;
+                            }
+                        }
+                        2 => {
+                            if neighbour_pixel.color == 3 {
+                                neighbour_pixel.color = 0;
+                                neighbour_pixel.intensity = self.max_intensity;
+                                self.buf[y][x].intensity =
+                                    min(self.buf[y][x].intensity + 1, self.max_intensity);
+                                // buf[y][x].intensity = 10;
+                            }
+                        }
+                        3 => {
+                            if neighbour_pixel.color == 1 {
+                                neighbour_pixel.color = 0;
+                                neighbour_pixel.intensity = self.max_intensity;
+                                self.buf[y][x].intensity =
+                                    min(self.buf[y][x].intensity + 1, self.max_intensity);
+                                // buf[y][x].intensity = 10;
+                            }
+                        }
+                        _ => {}
+                    }
 
-    // canvas.set_draw_color(Color::RGB(0, 0, 0));
+                    self.buf[neighbour_y as usize][neighbour_x as usize] = neighbour_pixel;
+                }
+            }
+        }
 
-    // for x in 0..buf[0].len() {
-    //     canvas.draw_line(
-    //         Point::new((x as i32) * block_size, (0 as i32) * block_size),
-    //         Point::new((x as i32) * block_size, (scr_height as i32) * block_size),
-    //     )?;
-    // }
+        for y in 0..self.buf.len() {
+            for x in 0..self.buf[0].len() {
+                let rect_color: Color;
 
-    // for y in 0..buf.len() {
-    //     canvas.draw_line(
-    //         Point::new((0 as i32) * block_size, (y as i32) * block_size),
-    //         Point::new((scr_width as i32) * block_size, (y as i32) * block_size),
-    //     )?;
-    // }
+                match self.buf[y][x].color {
+                    0 => rect_color = Color::RGBA(200, 200, 200, 0),
+                    1 => rect_color = Color::RGB(246, 122, 17),
+                    2 => rect_color = Color::RGB(208, 37, 37),
+                    3 => rect_color = Color::RGB(35, 119, 181),
+                    _ => rect_color = Color::RGB(20, 20, 20),
+                }
 
-    // canvas.present();
+                if self.buf[y][x].color != 0 {
+                    context.canvas.set_draw_color(rect_color);
+                    context.canvas.fill_rect(Rect::new(
+                        (x as i32) * context.block_size,
+                        (y as i32) * context.block_size,
+                        context.block_size as u32,
+                        context.block_size as u32,
+                    ))?;
+                }
+            }
+        }
+
+        context.canvas.set_draw_color(Color::RGB(0, 0, 0));
+
+        for x in 0..self.buf[0].len() {
+            context.canvas.draw_line(
+                Point::new(
+                    (x as i32) * context.block_size,
+                    (0 as i32) * context.block_size,
+                ),
+                Point::new(
+                    (x as i32) * context.block_size,
+                    (context.scr_height as i32) * context.block_size,
+                ),
+            )?;
+        }
+
+        for y in 0..self.buf.len() {
+            context.canvas.draw_line(
+                Point::new(
+                    (0 as i32) * context.block_size,
+                    (y as i32) * context.block_size,
+                ),
+                Point::new(
+                    (context.scr_width as i32) * context.block_size,
+                    (y as i32) * context.block_size,
+                ),
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn in_bounds(y: &i32, x: &i32, grid_h: &i32, grid_w: &i32) -> bool {
+        y >= &0 && x >= &0 && y < grid_h && x < grid_w
+    }
 }
